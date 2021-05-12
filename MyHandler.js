78e5f2e760fb059cf28b5c6a81dd748f3fa038bf
K-yuri <dbfl2989@mokwon.ac.kr>
2021-05-12 10:44:02 +0900

    add server info page.

--- MyHandler.js
+++ MyHandler.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
+const os = require('os');
 
 function start(res) {
     let body = '<head><meta charset ="UTF-8"/></head>';
     body += '<body><div>Hello, world! <br> I am in the cloud class.</div><br>';
-    body += '<div><a href"/hello">hello 페이지</a></div>';
-    body += '<div><a href"/wait">5초 대기 페이지</a></div>';
-    body += '<div><a href"/randomWait">무작위 대기 페이지</a></div>';
-    body += '<div><a href"/firstHtml">HTML 읽는 페이지</a></div>';
+    body += '<div><a href="/hello">hello 페이지</a></div>';
+    body += '<div><a href="/wait">5초 대기 페이지</a></div>';
+    body += '<div><a href="/randomWait">무작위 대기 페이지</a></div>';
+    body += '<div><a href="/firstHtml">HTML 읽는 페이지</a></div>';
+    body += '<div><a href="/firstHtml">Handler 없이 "/page"로 매핑하는 페이지</a></div>';
+    body += '<div><a href="/serverInfo:>Server 정보를 표시하는 페이지</a></div>';
     body += '</body>';
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(body);
@@ -50,9 +53,17 @@ function firstHtml(res) {
     htmlFile(res, './firstHtml.html');
 }
 
+function serverInfo(res) {
+    info = JSON.stringify(os.cpus());
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(info);
+    res.end();
+}
+
 exports.start = start;
 exports.hello = hello;
 exports.wait = wait;
 exports.randomWait = randomWait;
 exports.firstHtml = firstHtml;
 exports.htmlFile = htmlFile;
+exports.serverInfo = serverInfo;
